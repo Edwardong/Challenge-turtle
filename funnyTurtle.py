cb8de343b70c7b16de3a18313671ce47738f1bce
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2016-08-31 23:15:59 -0700

    combine two function since turtle isn't thread safe

--- funnyTurtle.py
+++ funnyTurtle.py
@@ -1,6 +1,7 @@
 import turtle
 import math
-
+from threading import Thread
+from time import sleep
 
 #draw four obliqueSquares firstly
 turtle.setup(800,600)
@@ -33,7 +34,8 @@ obliqueSquare(Edward,-dis/math.sqrt(2),2*dis/math.sqrt(2))
 obliqueSquare(Edward,dis/math.sqrt(2),0)
 obliqueSquare(Edward,-dis/math.sqrt(2),-2*dis/math.sqrt(2))
 turtle.update()
-#——————————————————————————————————————
+
+##############################################
 
 #define for points, draw a line through these points with no delay
 #a,b,c,d are the names of the points
@@ -42,224 +44,257 @@ stepsize=1/3
 
 
 def drawSquare1(t):
-	a=(-3*dis/math.sqrt(2),0)
-	b=(0,3*dis/math.sqrt(2))
-	c=(3*dis/math.sqrt(2),0)
-	d=(0,-3*dis/math.sqrt(2))
-	change1=(stepsize/math.sqrt(2),stepsize/math.sqrt(2))
-	a=list(a)
-	b=list(b)
-	c=list(c)
-	d=list(d)
-	change1=list(change1)
+	a0=(-3*dis/math.sqrt(2),0)
+	b0=(0,3*dis/math.sqrt(2))
+	c0=(3*dis/math.sqrt(2),0)
+	d0=(0,-3*dis/math.sqrt(2))
+	change01=(stepsize/math.sqrt(2),stepsize/math.sqrt(2))
+	a0=list(a0)
+	b0=list(b0)
+	c0=list(c0)
+	d0=list(d0)
+	change01=list(change01)
+
+
+	a1=(-2*dis/math.sqrt(2),dis/math.sqrt(2))
+	b1=(dis/math.sqrt(2),2*dis/math.sqrt(2))
+	c1=(2*dis/math.sqrt(2),-dis/math.sqrt(2))
+	d1=(-dis/math.sqrt(2),-2*dis/math.sqrt(2))
+	change11=(stepsize/math.sqrt(2),stepsize/math.sqrt(2))
+	a1=list(a1)
+	b1=list(b1)
+	c1=list(c1)
+	d1=list(d1)
+	change11=list(change11)
+
 	for i in range(steps):
+		t.clear()
 		turtle.tracer(0,0)
 		t.hideturtle()
 		t.up()
-		t.goto(a)
+		t.goto(a0)
 		t.down()
 		t.left(45)
-		t.goto(b)
+		t.goto(b0)
 		t.right(90)
-		t.goto(c)
+		t.goto(c0)
 		t.right(90)
-		t.goto(d)
+		t.goto(d0)
 		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]+change1[0],a[1]+change1[1]]
-		b=[b[0]+change1[0],b[1]-change1[1]]
-		c=[c[0]-change1[0],c[1]-change1[1]]
-		d=[d[0]-change1[0],d[1]+change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
-	for j in range(steps):
-		turtle.tracer(0,0)
-		t.hideturtle()
-		t.up()
-		t.goto(a)
-		t.down()
-		t.left(45)
-		t.goto(b)
-		t.right(90)
-		t.goto(c)
-		t.right(90)
-		t.goto(d)
-		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]+change1[0],a[1]-change1[1]]
-		b=[b[0]-change1[0],b[1]-change1[1]]
-		c=[c[0]-change1[0],c[1]+change1[1]]
-		d=[d[0]+change1[0],d[1]+change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
-	for k in range(steps):
-		turtle.tracer(0,0)
-		t.hideturtle()
-		t.up()
-		t.goto(a)
-		t.down()
-		t.left(45)
-		t.goto(b)
-		t.right(90)
-		t.goto(c)
-		t.right(90)
-		t.goto(d)
-		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]-change1[0],a[1]-change1[1]]
-		b=[b[0]-change1[0],b[1]+change1[1]]
-		c=[c[0]+change1[0],c[1]+change1[1]]
-		d=[d[0]+change1[0],d[1]-change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
-	for l in range(steps):
-		turtle.tracer(0,0)
-		t.hideturtle()
-		t.up()
-		t.goto(a)
-		t.down()
-		t.left(45)
-		t.goto(b)
-		t.right(90)
-		t.goto(c)
-		t.right(90)
-		t.goto(d)
-		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]-change1[0],a[1]+change1[1]]
-		b=[b[0]+change1[0],b[1]+change1[1]]
-		c=[c[0]+change1[0],c[1]-change1[1]]
-		d=[d[0]-change1[0],d[1]-change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
+		t.goto(a0)
+		#turtle.update()
+		#t.reset()
 
+		a0=[a0[0]+change01[0],a0[1]+change01[1]]
+		b0=[b0[0]+change01[0],b0[1]-change01[1]]
+		c0=[c0[0]-change01[0],c0[1]-change01[1]]
+		d0=[d0[0]-change01[0],d0[1]+change01[1]]
+		a0=tuple(a0)
+		b0=tuple(b0)
+		c0=tuple(c0)
+		d0=tuple(d0)
 
-def drawSquare2(t):
-	a=(-2*dis/math.sqrt(2),dis/math.sqrt(2))
-	b=(dis/math.sqrt(2),2*dis/math.sqrt(2))
-	c=(2*dis/math.sqrt(2),-dis/math.sqrt(2))
-	d=(-dis/math.sqrt(2),-2*dis/math.sqrt(2))
-	change1=(stepsize/math.sqrt(2),stepsize/math.sqrt(2))
-	a=list(a)
-	b=list(b)
-	c=list(c)
-	d=list(d)
-	change1=list(change1)
-	for m in range(steps):
-		turtle.tracer(0,0)
-		t.hideturtle()
 		t.up()
-		t.goto(a)
+		t.goto(a1)
 		t.down()
 		t.left(45)
-		t.goto(b)
+		t.goto(b1)
 		t.right(90)
-		t.goto(c)
+		t.goto(c1)
 		t.right(90)
-		t.goto(d)
+		t.goto(d1)
 		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]+change1[0],a[1]-change1[1]]
-		b=[b[0]-change1[0],b[1]-change1[1]]
-		c=[c[0]-change1[0],c[1]+change1[1]]
-		d=[d[0]+change1[0],d[1]+change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
-	for n in range(steps):
-		turtle.tracer(0,0)
-		t.hideturtle()
-		t.up()
-		t.goto(a)
-		t.down()
-		t.left(45)
-		t.goto(b)
-		t.right(90)
-		t.goto(c)
-		t.right(90)
-		t.goto(d)
-		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]-change1[0],a[1]-change1[1]]
-		b=[b[0]-change1[0],b[1]+change1[1]]
-		c=[c[0]+change1[0],c[1]+change1[1]]
-		d=[d[0]+change1[0],d[1]-change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
-	for o in range(steps):
-		turtle.tracer(0,0)
-		t.hideturtle()
-		t.up()
-		t.goto(a)
-		t.down()
-		t.left(45)
-		t.goto(b)
-		t.right(90)
-		t.goto(c)
-		t.right(90)
-		t.goto(d)
-		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]-change1[0],a[1]+change1[1]]
-		b=[b[0]+change1[0],b[1]+change1[1]]
-		c=[c[0]+change1[0],c[1]-change1[1]]
-		d=[d[0]-change1[0],d[1]-change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
-	for p in range(steps):
-		turtle.tracer(0,0)
-		t.hideturtle()
-		t.up()
-		t.goto(a)
-		t.down()
-		t.left(45)
-		t.goto(b)
-		t.right(90)
-		t.goto(c)
-		t.right(90)
-		t.goto(d)
-		t.right(90)
-		t.goto(a)
-		turtle.update()
-		t.reset()
-		a=[a[0]+change1[0],a[1]+change1[1]]
-		b=[b[0]+change1[0],b[1]-change1[1]]
-		c=[c[0]-change1[0],c[1]-change1[1]]
-		d=[d[0]-change1[0],d[1]+change1[1]]
-		a=tuple(a)
-		b=tuple(b)
-		c=tuple(c)
-		d=tuple(d)
+		t.goto(a1)
+		#turtle.update()
+		# t.reset()
+		a1=[a1[0]+change11[0],a1[1]-change11[1]]
+		b1=[b1[0]-change11[0],b1[1]-change11[1]]
+		c1=[c1[0]-change11[0],c1[1]+change11[1]]
+		d1=[d1[0]+change11[0],d1[1]+change11[1]]
+		a1=tuple(a1)
+		b1=tuple(b1)
+		c1=tuple(c1)
+		d1=tuple(d1)
 		
 
-Amy=turtle.Turtle()
+		turtle.update()
+		#sleep(0.05)
 
+
+	# for m in range(steps):
+	# 	turtle.tracer(0,0)
+	# 	t.hideturtle()
+
+	# for j in range(steps):
+	# 	turtle.tracer(0,0)
+	# 	t.hideturtle()
+	# 	t.up()
+	# 	t.goto(a)
+	# 	t.down()
+	# 	t.left(45)
+	# 	t.goto(b)
+	# 	t.right(90)
+	# 	t.goto(c)
+	# 	t.right(90)
+	# 	t.goto(d)
+	# 	t.right(90)
+	# 	t.goto(a)
+	# 	turtle.update()
+	# 	t.reset()
+	# 	a=[a[0]+change1[0],a[1]-change1[1]]
+	# 	b=[b[0]-change1[0],b[1]-change1[1]]
+	# 	c=[c[0]-change1[0],c[1]+change1[1]]
+	# 	d=[d[0]+change1[0],d[1]+change1[1]]
+	# 	a=tuple(a)
+	# 	b=tuple(b)
+	# 	c=tuple(c)
+	# 	d=tuple(d)
+	# for k in range(steps):
+	# 	turtle.tracer(0,0)
+	# 	t.hideturtle()
+	# 	t.up()
+	# 	t.goto(a)
+	# 	t.down()
+	# 	t.left(45)
+	# 	t.goto(b)
+	# 	t.right(90)
+	# 	t.goto(c)
+	# 	t.right(90)
+	# 	t.goto(d)
+	# 	t.right(90)
+	# 	t.goto(a)
+	# 	turtle.update()
+	# 	t.reset()
+	# 	a=[a[0]-change1[0],a[1]-change1[1]]
+	# 	b=[b[0]-change1[0],b[1]+change1[1]]
+	# 	c=[c[0]+change1[0],c[1]+change1[1]]
+	# 	d=[d[0]+change1[0],d[1]-change1[1]]
+	# 	a=tuple(a)
+	# 	b=tuple(b)
+	# 	c=tuple(c)
+	# 	d=tuple(d)
+	# for l in range(steps):
+	# 	turtle.tracer(0,0)
+	# 	t.hideturtle()
+	# 	t.up()
+	# 	t.goto(a)
+	# 	t.down()
+	# 	t.left(45)
+	# 	t.goto(b)
+	# 	t.right(90)
+	# 	t.goto(c)
+	# 	t.right(90)
+	# 	t.goto(d)
+	# 	t.right(90)
+	# 	t.goto(a)
+	# 	turtle.update()
+	# 	t.reset()
+	# 	a=[a[0]-change1[0],a[1]+change1[1]]
+	# 	b=[b[0]+change1[0],b[1]+change1[1]]
+	# 	c=[c[0]+change1[0],c[1]-change1[1]]
+	# 	d=[d[0]-change1[0],d[1]-change1[1]]
+	# 	a=tuple(a)
+	# 	b=tuple(b)
+	# 	c=tuple(c)
+	# 	d=tuple(d)
+
+	
+
+# def drawSquare2(t):
+# 	a1=(-2*dis/math.sqrt(2),dis/math.sqrt(2))
+# 	b1=(dis/math.sqrt(2),2*dis/math.sqrt(2))
+# 	c1=(2*dis/math.sqrt(2),-dis/math.sqrt(2))
+# 	d1=(-dis/math.sqrt(2),-2*dis/math.sqrt(2))
+# 	change1=(stepsize/math.sqrt(2),stepsize/math.sqrt(2))
+# 	a1=list(a1)
+# 	b1=list(b1)
+# 	c1=list(c1)
+# 	d1=list(d1)
+# 	change11=list(change11)
+
+# 	for n in range(steps):
+# 		turtle.tracer(0,0)
+# 		t.hideturtle()
+# 		t.up()
+# 		t.goto(a)
+# 		t.down()
+# 		t.left(45)
+# 		t.goto(b)
+# 		t.right(90)
+# 		t.goto(c)
+# 		t.right(90)
+# 		t.goto(d)
+# 		t.right(90)
+# 		t.goto(a)
+# 		turtle.update()
+# 		t.reset()
+# 		a=[a[0]-change1[0],a[1]-change1[1]]
+# 		b=[b[0]-change1[0],b[1]+change1[1]]
+# 		c=[c[0]+change1[0],c[1]+change1[1]]
+# 		d=[d[0]+change1[0],d[1]-change1[1]]
+# 		a=tuple(a)
+# 		b=tuple(b)
+# 		c=tuple(c)
+# 		d=tuple(d)
+# 	for o in range(steps):
+# 		turtle.tracer(0,0)
+# 		t.hideturtle()
+# 		t.up()
+# 		t.goto(a)
+# 		t.down()
+# 		t.left(45)
+# 		t.goto(b)
+# 		t.right(90)
+# 		t.goto(c)
+# 		t.right(90)
+# 		t.goto(d)
+# 		t.right(90)
+# 		t.goto(a)
+# 		turtle.update()
+# 		t.reset()
+# 		a=[a[0]-change1[0],a[1]+change1[1]]
+# 		b=[b[0]+change1[0],b[1]+change1[1]]
+# 		c=[c[0]+change1[0],c[1]-change1[1]]
+# 		d=[d[0]-change1[0],d[1]-change1[1]]
+# 		a=tuple(a)
+# 		b=tuple(b)
+# 		c=tuple(c)
+# 		d=tuple(d)
+# 	for p in range(steps):
+# 		turtle.tracer(0,0)
+# 		t.hideturtle()
+# 		t.up()
+# 		t.goto(a)
+# 		t.down()
+# 		t.left(45)
+# 		t.goto(b)
+# 		t.right(90)
+# 		t.goto(c)
+# 		t.right(90)
+# 		t.goto(d)
+# 		t.right(90)
+# 		t.goto(a)
+# 		turtle.update()
+# 		t.reset()
+# 		a=[a[0]+change1[0],a[1]+change1[1]]
+# 		b=[b[0]+change1[0],b[1]-change1[1]]
+# 		c=[c[0]-change1[0],c[1]-change1[1]]
+# 		d=[d[0]-change1[0],d[1]+change1[1]]
+# 		a=tuple(a)
+# 		b=tuple(b)
+# 		c=tuple(c)
+# 		d=tuple(d)
+
+# 	sleep(0.5)
+
+Amy=turtle.Turtle()
+# Douglas=turtle.Turtle()
 drawSquare1(Amy)
-drawSquare2(Amy)
+# drawSquare2(Douglas)
+#while True:
+# t1 = Thread(target = drawSquare1, args = (Amy,))
+# t1.start()
+# #t1.sleep(0.05)
+# t2 = Thread(target = drawSquare2, args = (Douglas,))
+# t2.start()
+	#t2.sleep(0.05)
